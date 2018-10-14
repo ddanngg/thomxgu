7b208208558b074c9a61f50f04240320a43ebff2
Nguyen An <andinh2601@gmail.com>
2018-10-14 19:07:23 +0700

    detail body page complete

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,16 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-	let ulSearchBox = document.querySelector('ul.search-box');
-	let searchBtn = document.querySelectorAll('ul.ul-header-right li');
-	let svgLogo = document.querySelector('svg#icon-thom_browne_logo');
-	let btnCancelSearch = document.querySelector('ul.search-box a.btnCancel');
+	let ulSearchBox = document.querySelector('ul.search-box'),
+	searchBtns = document.querySelectorAll('ul.ul-header-right li'),
+	svgLogo = document.querySelector('svg#icon-thom_browne_logo'),
+	btnCancelSearch = document.querySelector('ul.search-box a.btnCancel'),
+	inputCheck = document.querySelectorAll('.product-size ul li input');
 
-	searchBtn[1].addEventListener('click', () => {
+	searchBtns[1].addEventListener('click', () => {
 		ulSearchBox.classList.add('active');
 		svgLogo.classList.add('search-on');
 	});
-
 	btnCancelSearch.addEventListener('click', () => {
 		ulSearchBox.classList.remove('active');
 		svgLogo.classList.remove('search-on');
 	});
+	for (let x of inputCheck) {
+		x.onclick = () => {
+			for (let b of inputCheck) {
+				b.checked = false;
+			}
+			x.checked = true;
+		};
+	};
+
+	let imgActive = document.querySelector('.left-products-images img._detailImgActive'),
+	imgActiveMain = document.querySelector('.middle-products-images img'),
+	imgDetails = document.querySelectorAll('.left-products-images img');
+	imgActiveMain.src = imgActive.src;
+
+	for (let x of imgDetails) {
+		x.onclick = () => {
+			for (let y of imgDetails) {
+				y.classList.remove('_detailImgActive');
+			}
+			imgActiveMain.src = '';
+			x.classList.add('_detailImgActive');
+			imgActiveMain.src = x.src;
+		}
+	};
+
+
+
+
+
 }, false);
